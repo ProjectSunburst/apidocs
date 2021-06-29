988f74fbde8e9e315978c4c82af9d9996744ca3f
Nathan Curran <30569566+Focusvity@users.noreply.github.com>
2021-06-29 21:10:45 +1000

    API overview of the group system

--- package-search-index.js
+++ package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.focusvity.dev.projectsunburst.api"},{"l":"me.focusvity.dev.projectsunburst.api.ban"},{"l":"me.focusvity.dev.projectsunburst.api.player"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"me.focusvity.dev.projectsunburst.api"},{"l":"me.focusvity.dev.projectsunburst.api.ban"},{"l":"me.focusvity.dev.projectsunburst.api.permission"},{"l":"me.focusvity.dev.projectsunburst.api.player"}];updateSearchResults();
